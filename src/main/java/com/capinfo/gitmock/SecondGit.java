5a552161be313d19686ff8dfc11ae8fd5371a554
tianye <tianye>
2019-02-15 17:46:02 +0800

    name

--- src/main/java/com/capinfo/gitmock/SecondGit.java
+++ src/main/java/com/capinfo/gitmock/SecondGit.java
@@ -2,7 +2,9 @@ package com.capinfo.gitmock;
 
 public class SecondGit {
 
+    private static String name;
     public static void main(String[] args) {
         System.out.println("Second Git Java");
     }
+
 }
